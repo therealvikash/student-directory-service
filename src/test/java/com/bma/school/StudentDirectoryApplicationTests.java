3077f4a861a14ee2ee45a36089242dfbbb93209a
Bikash Yadav <bikashjadav333@gmail.com>
2023-02-23 13:28:45 +0530

    Initial Push

---
+++ src/test/java/com/bma/school/StudentDirectoryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bma.school;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudentDirectoryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
