3f03032105a179fe61dfa97e63a86b02fe9c8ed5
Bikash Yadav <bikashjadav333@gmail.com>
2023-02-23 23:27:17 +0530

    Updated build.gradle file & Created required packages

---
+++ src/main/java/com/bma/school/controller/BMASchoolController.java
@@ -0,0 +1,18 @@
+package com.bma.school.controller;
+
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class BMASchoolController {
+
+	private transient Integer val = 0;
+	
+	@GetMapping(value = "/")
+	public String getValue() {
+
+		String result = "Learnng how to do jenkins staging" + val;
+		return result;
+	}
+}
